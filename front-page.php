a030a0e321e8995ca72bfb43c29a97d3bd97df2b
Yarosh_IS7 <yaroshivan7@gmail.com>
2020-04-16 08:53:40 +0300

    1

---
+++ front-page.php
@@ -0,0 +1,22 @@
+<!--
+    Template name: Home
+-->
+
+<?php get_header(); ?>
+<?php $fields = get_fields(); ?>
+<?php $options = get_fields('option'); ?>
+
+<div class="page-home">
+
+    <!-- Section -->
+    <section class="section">
+        <div class="container">
+        
+        
+        </div>
+    </section>
+
+</div>
+
+<?php get_footer(); ?>
+
